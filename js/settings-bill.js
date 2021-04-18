3ab0e3626caf7c499351564af1a6216f5f41de81
V203 <vuyisamr203@gmail.com>
2021-04-18 16:49:55 +0200

    manjaro

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,30 +1,5 @@
-// get a reference to the _sms_ or call radio buttons
-
-// get refences to all the settings fields
-
-//get a reference to the add button
-
-//get a reference to the 'Update settings' button
-
-// create a variables that will keep track of all the settings
-
-// create a variables that will keep track of all three totals.
-
-//add an event listener for when the 'Update settings' button is pressed
-
-//add an event listener for when the add button is pressed
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the call / _sms_ total
-// * add the appropriate value to the overall total
-// * add nothing for invalid values that is not 'call' or '_sms_'.
-// * display the latest total on the screen.
-// * check the value thresholds and display the total value in the right color.
 
 {
-
-
-
     var callCostSetting = document.querySelector(".callCostSetting");
     var smsCostSetting = document.querySelector(".smsCostSetting");
 
@@ -37,7 +12,6 @@
     var criticalLevelSetting = document.querySelector(".criticalLevelSetting");
     var updateSettingsbtn = document.querySelector(".button-primary-updateSettings");
 
-
     var call = 0;
     var sms = 0;
     var total = 0;
@@ -47,8 +21,6 @@
 
     var _call_ = 0;
     var _sms_ = 0;
-
-
 
     updateSettingsbtn.addEventListener("click", function () {
         totalSettings.classList.remove("danger")
@@ -61,8 +33,6 @@
         critLevel = parseFloat(criticalLevelSetting.value);
 
 
-
-
         if (total >= warnLevel) {
             totalSettings.classList.add("warning")
         }
@@ -71,10 +41,6 @@
         }
         smsTotalSettings.innerHTML = _sms_.toFixed(2);
         callTotalSettings.innerHTML = _call_.toFixed(2);
-
-
-   
-
 
         testNum();
 
@@ -130,13 +96,7 @@
 
 
     var testNum = () => {
-        // if(isNaN((call && sms)&&(warnLevel && critLevel))){
-        //     alert("please Enter a Digit in all the settings input fields.")
-        //     smsCostSetting.value = ""
-        //     callCostSetting.value ="";
-        //     warningLevelSetting.value = "";
-        //     criticalLevelSetting.value = "";
-        // }
+   
         if (isNaN(call)) {
 
             callCostSetting.value = "";
@@ -156,13 +116,7 @@
             alert("please Enter a Digit in all the ctitical settings input fields.")
         }
 
-
-
-
-
-
-
-        // Math.sign((call && sms)&&(warnLevel && critLevel)) == -1 ? alert("You cant input negative values"):undefined;
+   
         if (Math.sign(call) === -1) {
             callCostSetting.value = "";
             alert("You cannot input negative values in Call cost settings.");
@@ -182,15 +136,7 @@
             criticalLevelSetting.value = "";
             alert("You cannot input negative values in critical level settings.");
         }
-        // callTotalSettings.innerHTML = ""
-        // smsTotalSettings.innerHTML = ""
-        //alert(typeof call)
-
-        
+ 
    }
-
-
-
-
 
 }
