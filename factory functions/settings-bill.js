4c60249bb6bbbf65049532db4733ffafdeecc6a6
V203 <vuyisamr203@gmail.com>
2021-05-04 21:49:18 +0200

    manjaro

---
+++ factory functions/settings-bill.js
@@ -0,0 +1,141 @@
+
+
+function billWithSettings() {
+    var call = 0;
+    var callTotal = 0;
+
+    var sms = 0;
+    var smsTotal = 0;
+
+    var total = 0;
+    var CallSmsTotal = 0;
+
+    var warn = 0;
+    var crit = 0;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+
+    function setCall(call_) {
+        return call += call_;
+
+    }
+
+    function getCall() {
+        return this.call;
+    }
+    function getCallTotal() {
+        return callTotal;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function setSms(sms_) {
+        return sms += sms_
+    }
+
+    function getSms() {
+        return sms;
+    }
+    function getSmsTotal() {
+        return smsTotal
+    }
+    function togLevel(){
+        if (total >= warnLevel) {
+            totalSettings.classList.add("warning")
+        }
+        if (total >= critLevel) {
+            totalSettings.classList.remove("danger")
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function getTotal() {
+        return total = call + sms;
+    }
+
+    function getCallSmsTotal() {
+        return CallSmsTotal = callTotal + smsTotal;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    function makeCall() {
+        if(!hasReachedCriticalLevel()){
+            return callTotal += call;
+        }
+        
+    }
+
+    function sendSms() {
+        if(!hasReachedCriticalLevel()){
+            return smsTotal += sms
+        }
+        
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function setWarn(warn_) {
+        return warn += warn_
+
+    }
+    function getWarn() {
+        return warn
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function setCrit(crit_) {
+        return crit += crit_;
+    }
+    function getCrit() {
+        return crit;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function totalClassName() {
+        if (hasReachedCriticalLevel()) {
+            return "critical";
+        }
+        
+        if (getCallSmsTotal() >= getWarn()) {
+            return "warning";
+        }
+        
+        
+        
+    }
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function hasReachedCriticalLevel(){
+        return getCallSmsTotal() >= getCrit();
+    }
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    return {
+        setCall,
+        getCall,
+        getCallTotal,
+        setSms,
+        getSms,
+        getSmsTotal,
+        getTotal,
+        getCallSmsTotal,
+        makeCall,
+        sendSms,
+        setWarn,
+        getWarn,
+        setCrit,
+        getCrit,
+        totalClassName,
+        hasReachedCriticalLevel,
+        togLevel
+
+
+
+    }
+
+
+
+}
--- js/text-bill.js
+++ js/text-bill.js
@@ -1,15 +1,3 @@
-// get a reference to the textbox where the bill type is to be entered
-
-//get a reference to the add button
-
-//create a variable that will keep track of the total bill
-
-//add an event listener for when the add button is pressed
-
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
 
 
 let callTotalOne = document.querySelector(".callTotalOne");
@@ -21,69 +9,17 @@ let totalOne = document.querySelector(".totalOne");
 
 let btn_ = document.querySelector(".button-primary.addToBillBtn")
 
-
+let tb = textbill()
 
 btn_.addEventListener("click",function(){
-    let tb = textbill()
-    // calculate_(billTypeText.value)
-    //total(call_,sms_)
-    tb._add(billTypeText.value)
+    
+
+    tb.add(billTypeText.value)
 
     callTotalOne.innerHTML = tb.getCall()
     smsTotalOne.innerHTML  = tb.getSms();
     totalOne.innerHTML = tb.smsCallTotal()
     totalOne.classList.add(tb.totalClassName())
-
-// btn_.addEventListener("click", function () {
-//     let tbill = textbill();
-    
-//     alert(tbill.add(billTypeText.value))
-// });
-
-
-// add = (x) => {
-
-
-//      x = billTypeText.value.trim();
-//     if (x == "") {
-//         alert("Please enter 'call' or 'sms' in the billtype text field")
-//     }
-
-//     if (x === "call") {
-//         call_ += 2.75
-
-//     }
-
-//     else if (x === "sms") {
-
-//         sms_ += 0.75
-//     }
-   
-//     callTotalOne.innerHTML = call_.toFixed(2);
-//     smsTotalOne.innerHTML = sms_.toFixed(2);
-//     total_ = call_ + sms_
-//     totalOne.innerHTML = total_.toFixed(2) ;
-
-//     if (total_ >= 50) {
-//         totalOne.classList.add("danger")
-//     } else if (total_ >= 30) {
-//         totalOne.classList.add("warning")
-//     }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
 
